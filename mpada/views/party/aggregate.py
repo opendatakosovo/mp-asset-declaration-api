90024b503d663939091195806fbba7136aba092b
legeorges <georges.labreche@gmail.com>
2014-09-09 04:46:57 -0700

    Added more fields in aggregate call for assets sum

--- mpada/views/party/aggregate.py
+++ mpada/views/party/aggregate.py
@@ -22,75 +22,7 @@ class PartyAggregate(View):
 		} 
 
 		# Group.
-		group = {
-			'$group':{
-				'_id':'$year',
-
-				# Total real estate assets.
-				'realEstateIndividual':{
-					'$sum':'$realEstate.individual'
-				},
-				'realEstateJoint':{
-					'$sum':'$realEstate.joint'
-				},
-
-				# Total movable assets.
-				'movableIndividual':{
-					'$sum':'$movable.individual'
-				},
-				'movableJoint':{
-					'$sum':'$movable.joint'
-				},
-
-				# Total individual assets.
-				'sharesIndividual':{
-					'$sum':'$shares.individual'
-				},
-				'sharesJoint':{
-					'$sum':'$shares.joint'
-				},
-
-				# Total bonds assets.
-				'bondsIndividual':{
-					'$sum':'$bonds.individual'
-				},
-				'bondsJoint':{
-					'$sum':'$bonds.joint'
-				},
-
-				# Total cash assets.
-				'cashIndividual':{
-					'$sum':'$cash.individual'
-				},
-				'cashJoint':{
-					'$sum':'$cash.joint'
-				},
-
-				# Total debs or outstanding assets.
-				'debtsOrOutstandingIndividual':{
-					'$sum':'$debtsOrOutstanding.individual'
-				},
-				'debtsOrOutstandingJoint':{
-					'$sum':'$debtsOrOutstanding.joint'
-				},
-
-				# Total regular annual salary assets.
-				'annualSalaryRegularIndividual':{
-					'$sum':'$annualSalary.regular.individual'
-				},
-				'annualSalaryRegularJoint':{
-					'$sum':'$annualSalary.regular.joint'
-				},
-
-				# Total honorarium annual salary assets.
-				'annualSalaryHonorariumsIndividual':{
-					'$sum':'$annualSalary.honorariums.individual'
-				},
-				'annualSalaryHonorariumsJoint':{
-					'$sum':'$annualSalary.honorariums.joint'
-				},
-			}
-		}
+		group = get_group()
 
 		# Sort.
 		sort = { 
@@ -100,28 +32,14 @@ class PartyAggregate(View):
 		}
 
 		# Projection.
-		project = {
-			'$project':{
-				'_id': 0,
-				'realEstateIndividual': 0,
-				'realEstateJoint': 0,
-				'year': '$_id',
-				'realEstate':{
-					'individual': '$realEstateIndividual',
-					'joint': '$realEstateJoint'
-				},
-				'movable':{
-					'individual': '$movableIndividual',
-					'joint': '$movableJoint'
-				}		
-			}
-		}
+		project = get_projection()
 
 		# Execute query.
 		declarations = mongo.db.mpassetdeclarations.aggregate([
 			query,
 			group,
-			sort
+			sort,
+			project
 		])
 
 		# Build response object.			
@@ -129,3 +47,175 @@ class PartyAggregate(View):
 
 		# Return response.
 		return resp
+
+def get_group():
+	''' Build and return the group object to be used in aggregation pipeline.
+	'''
+
+	group =	{
+		'$group':{
+			'_id':'$year',
+
+			# Total real estate assets.
+			'realEstateIndividual':{
+				'$sum':'$realEstate.individual'
+			},
+			'realEstateJoint':{
+				'$sum':'$realEstate.joint'
+			},
+			'realEstateJoint':{
+				'$sum':'$realEstate.total'
+			},
+
+			# Total movable assets.
+			'movableIndividual':{
+				'$sum':'$movable.individual'
+			},
+			'movableJoint':{
+				'$sum':'$movable.joint'
+			},
+			'movableTotal':{
+				'$sum':'$movable.total'
+			},
+
+			# Total individual assets.
+			'sharesIndividual':{
+				'$sum':'$shares.individual'
+			},
+			'sharesJoint':{
+				'$sum':'$shares.joint'
+			},
+			'sharesTotal':{
+				'$sum':'$shares.total'
+			},
+
+			# Total bonds assets.
+			'bondsIndividual':{
+				'$sum':'$bonds.individual'
+			},
+			'bondsJoint':{
+				'$sum':'$bonds.joint'
+			},
+			'bondsTotal':{
+				'$sum':'$bonds.total'
+			},
+
+			# Total cash assets.
+			'cashIndividual':{
+				'$sum':'$cash.individual'
+			},
+			'cashJoint':{
+				'$sum':'$cash.joint'
+			},
+			'cashTotal':{
+				'$sum':'$cash.total'
+			},
+
+			# Total debs or outstanding assets.
+			'debtsOrOutstandingIndividual':{
+				'$sum':'$debtsOrOutstanding.individual'
+			},
+			'debtsOrOutstandingJoint':{
+				'$sum':'$debtsOrOutstanding.joint'
+			},
+			'debtsOrOutstandingTotal':{
+				'$sum':'$debtsOrOutstanding.total'
+			},
+
+			# Total regular annual salary assets.
+			'annualSalaryRegularIndividual':{
+				'$sum':'$annualSalary.regular.individual'
+			},
+			'annualSalaryRegularJoint':{
+				'$sum':'$annualSalary.regular.joint'
+			},
+			'annualSalaryRegularTotal':{
+				'$sum':'$annualSalary.regular.total'
+			},
+
+			# Total honorarium annual salary assets.
+			'annualSalaryHonorariumsIndividual':{
+				'$sum':'$annualSalary.honorariums.individual'
+			},
+			'annualSalaryHonorariumsJoint':{
+				'$sum':'$annualSalary.honorariums.joint'
+			},
+			'annualSalaryHonorariumsTotal':{
+				'$sum':'$annualSalary.honorariums.total'
+			},
+
+			# Totals
+			'totalIndividual':{
+				'$sum':'$totals.individual'
+			},
+			'totalJoint':{
+				'$sum':'$totals.joint'
+			},
+			'total':{
+				'$sum':'$totals.total'
+			}
+		}
+	}
+
+	return group
+
+
+def get_projection():
+	''' Get the projection object to be used in aggregation pipeline.
+	'''
+
+	project = {
+		'$project':{
+			'_id': 0,
+			'year': '$_id',
+			'realEstate':{
+				'individual': '$realEstateIndividual',
+				'joint': '$realEstateJoint',
+				'total': '$realEstateTotal'
+			},
+			'movable':{
+				'individual': '$movableIndividual',
+				'joint': '$movableJoint',
+				'total': '$movableTotal'
+			},
+			'shares':{
+				'individual': '$sharesIndividual',
+				'joint': '$sharesJoint',
+				'total': '$sharesTotal'
+			},
+			'bonds':{
+				'individual': '$bondsIndividual',
+				'joint': '$bondsJoint',
+				'total': '$bondsTotal'
+			},
+			'cash':{
+				'individual': '$cashIndividual',
+				'joint': '$cashJoint',
+				'total': '$cashTotal'
+			},
+			'debtsOrOutstanding':{
+				'individual': '$debtsOrOutstandingIndividual',
+				'joint': '$debtsOrOutstandingJoint',
+				'total': '$debtsOrOutstandingTotal'
+			},
+			'annualSalary':{
+				'regular':{
+					'individual': '$annualSalaryRegularIndividual',
+					'joint': '$annualSalaryRegularJoint',
+					'total': '$annualSalaryRegularTotal',
+				},
+				'honorariums':{
+					'individual': '$annualSalaryHonorariumsIndividual',
+					'joint': '$annualSalaryHonorariumsJoint',
+					'total': '$annualSalaryHonorariumsTotal',
+				},
+				'totals':{
+					'individual': '$totalIndividual',
+					'joint': '$totalJoint',
+					'total': '$total'
+				}
+			}
+		}
+	}
+
+	return project
